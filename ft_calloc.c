ee593e67845513232c3e82bcc560151e0d6c42c5
Bastien Guillaume <bguillau@e2r12p23.clusters.42paris.fr>
2022-11-24 11:37:14 +0100

    calloc mod

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: bguillau <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 11:40:09 by bguillau          #+#    #+#             */
-/*   Updated: 2022/11/22 15:21:25 by bguillau         ###   ########.fr       */
+/*   Updated: 2022/11/23 12:15:36 by bguillau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	void	*ptr;
 
 	ptr = NULL;
-	if (nmemb <= SIZE_MAX / size)
+	if (nmemb * size == 0 || nmemb <= SIZE_MAX / size)
 		ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (NULL);
